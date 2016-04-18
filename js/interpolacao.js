2f23deab7a9770b4ce6e009e366791443844b151
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2016-04-18 09:32:23 -0300

    Refatorado para objeto

--- js/interpolacao.js
+++ js/interpolacao.js
@@ -6,76 +6,85 @@ let d3 = require('d3');
 window.d3 = d3;
 let functionPlot = require('function-plot');
 
-let points = [];
+class GraficoInterpolador {
+    constructor(elemento, largura, altura, elementoTabela) {
+        this.polinomios = [];
+        this.pontos = [];
+        this.opcoes = this.obterOpcoesIniciais(elemento, largura, altura);
+        this.instancia = this.definirInstancia(this.opcoes);
+        this.registrarEventoClick();
+        this.elementoTabela = d3.select(elementoTabela);
+    }
+    registrarEventoClick() {
+        let self = this;
+        self.instancia.canvas.on('click', function () {
+            let xScale = self.instancia.meta.xScale;
+            let yScale = self.instancia.meta.yScale;
+            let coordenadas = d3.mouse(this);
+            let ponto = [d3.round(xScale.invert(coordenadas[0]), 3), d3.round(yScale.invert(coordenadas[1]), 3)];
+            self.pontos.push(ponto);
+            let polinomio = self.gerarInterpolacao();
+            self.definirInstancia(self.opcoes);
+            self.novoPontoTabela(self.pontos.length, ponto[0], ponto[1], polinomio);
+        });
+    }
+    novoPontoTabela(n, x, y, polinomio) {
+        let tr = this.elementoTabela.append('tr');
+        tr.append('td').text(n);
+        tr.append('td').text(x);
+        tr.append('td').text(y);
+        tr.append('td').text(polinomio);
+    }
+    gerarInterpolacao() {
+        let interpolador = interpolar(this.pontos, 5);
+        let polinomioInterpolador = interpolador.p;
+        let funcaoInterpoladora = interpolador.f;
 
-let options = {
-    target: '#grafico',
-    width: 900,
-    height: 400,
-    grid: true,
-    disableZoom: true,
-    xAxis: {
-        label: 'eixo - x',
-        domain: [-20,20]
-    },
-    yAxis: {
-        label: 'eixo - y',
-        domain: [-10,10]
-    },
-    data: [{
-        fnType: 'points',
-        sampler: 'builtIn',
-        points: points,
-        graphType: 'scatter',
-        color: 'black',
-        attr: {
-            r: '2px',
-        },
-        skipTip: true
-    }],
-};
-let instance = functionPlot(options);
-let canvas = instance.canvas;
-let xScale = instance.meta.xScale;
-let yScale = instance.meta.yScale;
-
-canvas.on('click', adicionarPonto);
-
-let tbodyPontos = d3.select('tbody#pontos');
-
-function adicionarPonto() {
-    let coord = d3.mouse(this);
-    let point = [d3.round(xScale.invert(coord[0]), 3), d3.round(yScale.invert(coord[1]),3)];
-    points.push(point);
-    let polinomio = novaInterpolacao();
-    functionPlot(options);
-    novoPontoTabela(points.length, point[0], point[1], polinomio);
+        this.opcoes.data.push({
+            fnType: 'linear',
+            graphType: 'polyline',
+            color: 'teal',
+            fn: function (p) {
+                return funcaoInterpoladora(p.x);
+            },
+            skipTip: true
+        });
+        return polinomioInterpolador;
+    }
+    definirInstancia(opcoes) {
+        return functionPlot(opcoes);
+    }
+    obterOpcoesIniciais(elemento, largura, altura) {
+        return {
+            target: elemento,
+            width: largura,
+            height: altura,
+            grid: true,
+            disableZoom: true,
+            xAxis: {
+                label: 'eixo - x',
+                domain: [-20, 20]
+            },
+            yAxis: {
+                label: 'eixo - y',
+                domain: [-10, 10]
+            },
+            data: [{
+                fnType: 'points',
+                sampler: 'builtIn',
+                points: this.pontos,
+                graphType: 'scatter',
+                color: 'black',
+                attr: {
+                    r: '2px',
+                },
+                skipTip: true
+            }],
+        };
+    }
 }
 
-function novaInterpolacao() {
-    let interpolador = interpolar(points, 5);
-    let polinomioInterpolador = interpolador.p;
-    let funcaoInterpoladora = interpolador.f;
-    
-    options.data.push({
-        fnType: 'linear',
-        graphType: 'polyline',
-        color: 'teal',
-        fn: function(p) {
-            return funcaoInterpoladora(p.x);
-        },
-        skipTip: true
-    });
-    return polinomioInterpolador;
-}
-
-function novoPontoTabela(n, x, y, polinomio) {
-    let tr = tbodyPontos.append('tr');
-    tr.append('td').text(n);
-    tr.append('td').text(x);
-    tr.append('td').text(y);
-    tr.append('td').text(polinomio);
-}
+let grafico = new GraficoInterpolador('#grafico', 900, 400, 'tbody#pontos');
 },{"../lib/interpolacao/interpolar_meg":2,"d3":9,"function-plot":24}],2:[function(require,module,exports){
 'use strict';
 
